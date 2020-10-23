9a39c797171bf22fe0900014a11eeb8d79a80080
Believer <longqh3@outlook.com>
2020-10-23 22:38:51 +0800

    Create demo.php

---
+++ PHP/demo.php
@@ -0,0 +1,49 @@
+<form>
+ <input type="text" name="num1">
+
+ <select name="fh">
+ <option value="jia"> + </option>
+ <option value="jian"> - </option>
+ <option value="c"> x </option>
+ <option value="chu"> / </option>
+ <option value="qy"> % </option>
+
+ </select>
+
+ <input type="text" name="num2">
+
+ <input type="submit" value="运算" />
+
+
+</form>
+
+<?php
+
+ $num1 = $_GET['num1'];
+ $num2 = $_GET['num2'];
+ $fh = $_GET['fh'];
+
+ if(!is_numeric($num1) || !is_numeric($num2)){
+
+ echo '请输入数值类型';
+ }
+
+ if($fh == 'jia'){
+ echo $num1 . '+' . $num2 . '=' . ($num1+$num2);
+ }
+
+ if($fh=='jian'){
+ echo $num1 . '-' . $num2 . '=' . ($num1-$num2);
+ }
+
+ if($fh=='c'){
+ echo $num1 . 'x' . $num2 . '=' . ($num1*$num2);
+ }
+ if($fh=='chu'){
+ echo $num1 . '/' . $num2 . '=' . ($num1/$num2);
+ }
+ if($fh=='qy'){
+ echo $num1 . '%' . $num2 . '=' . ($num1%$num2);
+ }
+
+?>
